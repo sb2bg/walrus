d3fea8f1f3a3ab38a86589dc23892e3ac12d926c
sb2bg <sullivanbognar@gmail.com>
2022-11-26 14:28:16 -0600

    Change to accessors

--- src/source_ref.rs
+++ src/source_ref.rs
@@ -1,11 +1,19 @@
-#[derive(Debug, PartialEq, Eq, Hash, PartialOrd, Ord)] // todo: remove PartialEq, Eq, Hash, PartialOrd, Ord
+#[derive(Debug)]
 pub struct SourceRef<'a> {
-    pub source: &'a str,
-    pub filename: &'a str,
+    source: &'a str,
+    filename: &'a str,
 }
 
 impl<'a> SourceRef<'a> {
     pub fn new(filename: &'a str, source: &'a str) -> Self {
         Self { source, filename }
+    }
+
+    pub fn source(&self) -> &'a str {
+        self.source
+    }
+
+    pub fn filename(&self) -> &'a str {
+        self.filename
     }
 }
