7768a6e762ff5a66146c296572cf5d54a74a9ef7
sb2bg <sullivanbognar@gmail.com>
2022-11-26 14:27:57 -0600

    Add collect AST method to only run loop in debug mode

--- src/program.rs
+++ src/program.rs
@@ -1,5 +1,5 @@
-use crate::ast::Node;
-use crate::error::err_mapper;
+use crate::ast::{Node, NodeKind};
+use crate::error::parser_err_mapper;
 use crate::grammar::ProgramParser;
 use crate::interpreter::Interpreter;
 use crate::source_ref::SourceRef;
@@ -21,27 +21,34 @@ impl<'a> Program<'a> {
     }
 
     pub fn run(&mut self) -> GlassResult {
-        let filename = self.global_interpreter.source_ref.filename;
-        let src = self.global_interpreter.source_ref.source;
+        let filename = self.global_interpreter.source_ref().filename();
+        let src = self.global_interpreter.source_ref().source();
 
         debug!("Read {} bytes from '{}'", &src.len(), filename);
 
         let ast = *self
             .parser
             .parse(src)
-            .map_err(|err| err_mapper(err, filename, src))?;
+            .map_err(|err| parser_err_mapper(err, filename, src))?;
 
         debug!("AST size > {:.2} KB", ast.get_heap_size() as f64 / 1024.0);
-
-        if let Node::Statement(stmts) = &ast {
-            for stmt in stmts {
-                debug!("Statement > {:?}", stmt);
-            }
-        }
+        debug!("AST > {:?}", self.collect_ast(ast.kind()));
 
         let res = self.global_interpreter.interpret(ast)?;
         debug!("Interpreted > {:?}", res);
 
         return Ok(());
+    }
+
+    fn collect_ast(&'a self, ast: &'a NodeKind) -> Vec<&Box<Node>> {
+        let mut nodes = vec![];
+
+        if let NodeKind::Statement(stmts) = ast {
+            for stmt in stmts {
+                nodes.push(stmt);
+            }
+        }
+
+        nodes
     }
 }
