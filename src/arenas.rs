38c0746c246a24c07a326457e470770c6a91e436
sb2bg <sullivanbognar@gmail.com>
2022-12-11 14:37:37 -0600

    Use arenas to alloc and operate on refs

---
+++ src/arenas.rs
@@ -0,0 +1,73 @@
+use crate::ast::Node;
+use crate::error::WalrusError;
+use crate::value::Value;
+use slotmap::{new_key_type, DenseSlotMap};
+use std::collections::BTreeMap;
+
+new_key_type! {
+    pub struct ListKey;
+    pub struct DictKey;
+    pub struct StringKey;
+    pub struct FuncKey;
+}
+
+type ArenaResult<T> = Result<T, WalrusError>;
+
+// todo: maybe instead of this, we can use a single slotmap
+// and use a different enum to differentiate between the
+// types stored by value and the types stored by key
+pub struct ValueHolder {
+    dict_slotmap: DenseSlotMap<DictKey, BTreeMap<Value, Value>>,
+    list_slotmap: DenseSlotMap<ListKey, Vec<Value>>,
+    string_slotmap: DenseSlotMap<StringKey, String>,
+    function_slotmap: DenseSlotMap<FuncKey, (String, Vec<String>, Node)>,
+}
+
+impl ValueHolder {
+    pub fn new() -> Self {
+        Self {
+            dict_slotmap: DenseSlotMap::with_key(),
+            list_slotmap: DenseSlotMap::with_key(),
+            string_slotmap: DenseSlotMap::with_key(),
+            function_slotmap: DenseSlotMap::with_key(),
+        }
+    }
+
+    pub fn insert_dict(&mut self, dict: BTreeMap<Value, Value>) -> DictKey {
+        self.dict_slotmap.insert(dict)
+    }
+
+    pub fn insert_list(&mut self, list: Vec<Value>) -> ListKey {
+        self.list_slotmap.insert(list)
+    }
+
+    pub fn insert_string(&mut self, string: String) -> StringKey {
+        self.string_slotmap.insert(string)
+    }
+
+    pub fn insert_function(&mut self, name: String, args: Vec<String>, body: Node) -> FuncKey {
+        self.function_slotmap.insert((name, args, body))
+    }
+
+    pub fn get_dict(&self, key: DictKey) -> ArenaResult<&BTreeMap<Value, Value>> {
+        Self::check(self.dict_slotmap.get(key))
+    }
+
+    pub fn get_list(&self, key: ListKey) -> ArenaResult<&Vec<Value>> {
+        Self::check(self.list_slotmap.get(key))
+    }
+
+    pub fn get_string(&self, key: StringKey) -> ArenaResult<&String> {
+        Self::check(self.string_slotmap.get(key))
+    }
+
+    pub fn get_function(&self, key: FuncKey) -> ArenaResult<&(String, Vec<String>, Node)> {
+        Self::check(self.function_slotmap.get(key))
+    }
+
+    fn check<T>(result: Option<T>) -> Result<T, WalrusError> {
+        result.ok_or(WalrusError::UnknownError {
+            message: "Attempt to access already released reference value".into(),
+        })
+    }
+}
--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,27 +1,32 @@
+use crate::arenas::ValueHolder;
 use crate::ast::{Node, NodeKind, Op};
-use crate::error::GlassError::UndefinedVariable;
-use crate::error::{interpreter_err_mapper, GlassError};
+use crate::error::WalrusError::UndefinedVariable;
+use crate::error::{interpreter_err_mapper, WalrusError};
 use crate::scope::Scope;
 use crate::source_ref::SourceRef;
 use crate::span::Span;
 use crate::value::Value;
-use log::debug;
+use crate::value::Value::Function;
+use float_ord::FloatOrd;
 use std::collections::BTreeMap;
-use std::mem::discriminant;
 use uuid::Uuid;
 
 pub struct Interpreter<'a> {
     scope: Scope<'a>,
     source_ref: SourceRef<'a>,
+    returnable: bool,
+    arenas: ValueHolder,
 }
 
-pub type InterpreterResult = Result<Value, GlassError>;
+pub type InterpreterResult<'a> = Result<Value, WalrusError>;
 
 impl<'a> Interpreter<'a> {
-    pub fn new(source_ref: SourceRef<'a>) -> Self {
+    pub fn new(source_ref: SourceRef<'a>, returnable: bool) -> Self {
         Self {
             scope: Scope::new(),
             source_ref,
+            returnable,
+            arenas: ValueHolder::new(),
         }
     }
 
@@ -29,11 +34,7 @@ impl<'a> Interpreter<'a> {
         &self.source_ref
     }
 
-    pub fn interpret(&mut self, ast: Node) -> InterpreterResult {
-        self.visit(ast)
-    }
-
-    fn visit(&mut self, node: Node) -> InterpreterResult {
+    pub fn interpret(&mut self, node: Node) -> InterpreterResult {
         // fixme: using copy to avoid borrow checker error, but this is probably not the best way to do this
         let span = *node.span();
 
@@ -44,14 +45,17 @@ impl<'a> Interpreter<'a> {
             NodeKind::Int(num) => Ok(Value::Int(num)),
             NodeKind::Float(num) => Ok(Value::Float(num)),
             NodeKind::Bool(boolean) => Ok(Value::Bool(boolean)),
-            NodeKind::String(string) => Ok(Value::String(string)),
+            NodeKind::String(string) => {
+                let key = self.arenas.insert_string(string);
+                Ok(Value::String(key))
+            }
             NodeKind::Dict(dict) => self.visit_dict(dict),
             NodeKind::List(list) => self.visit_list(list),
             NodeKind::FunctionDefinition(name, args, body) => self.visit_fn_def(name, args, *body),
             NodeKind::AnonFunctionDefinition(args, body) => self.visit_anon_fn_def(args, *body),
             NodeKind::Ident(ident) => self.visit_var(ident, span),
             NodeKind::Void => Ok(Value::Void),
-            node => Err(GlassError::UnknownError {
+            node => Err(WalrusError::UnknownError {
                 message: format!("Unknown node: {:?}", node),
             }),
         }
@@ -59,59 +63,68 @@ impl<'a> Interpreter<'a> {
 
     fn visit_statements(&mut self, nodes: Vec<Box<Node>>) -> InterpreterResult {
         for node in nodes {
-            let should_return = discriminant(node.kind())
-                == discriminant(&NodeKind::Return(Box::new(Node::new(
-                    NodeKind::Void,
-                    Span(0, 0),
-                )))); // fixme: this is such a hack
+            match *node {
+                Node {
+                    kind: NodeKind::Return(ret),
+                    ..
+                } => {
+                    // if !self.returnable {
+                    //     return Err(WalrusError::ReturnOutsideFunction {
+                    //         span: node.span(),
+                    //         src: self.source_ref.source().into(),
+                    //         filename: self.source_ref.filename().into(),
+                    //     });
+                    // }
 
-            let res = self.visit(*node)?;
-            debug!("{}", res);
-
-            if should_return {
-                return Ok(res);
-            }
+                    return self.interpret(*ret);
+                }
+                _ => self.interpret(*node)?,
+            };
         }
 
         Ok(Value::Void)
     }
 
     fn visit_bin_op(&mut self, left: Node, op: Op, right: Node, span: Span) -> InterpreterResult {
-        let left_res = self.visit(left)?;
-        let right_res = self.visit(right)?;
+        let left_res = self.interpret(left)?;
+        let right_res = self.interpret(right)?;
 
         match op {
-            Op::Add => left_res.add(right_res),
-            Op::Sub => left_res.sub(right_res),
-            Op::Mul => left_res.mul(right_res),
-            Op::Div => left_res.div(right_res),
-            Op::Mod => left_res.rem(right_res),
-            Op::Pow => left_res.pow(right_res),
-            Op::Equal => left_res.eq(right_res),
-            Op::NotEqual => left_res.ne(right_res),
-            Op::Less => left_res.lt(right_res),
-            Op::LessEqual => left_res.lt(right_res),
-            Op::Greater => left_res.gt(right_res),
-            Op::GreaterEqual => left_res.gt(right_res),
-            Op::And => left_res.and(right_res),
-            Op::Or => left_res.or(right_res),
-            Op::Not => Err(GlassError::UnknownError {
+            Op::Add => self.add(left_res, right_res, span),
+            // Op::Sub => left_res.sub(right_res),
+            // Op::Mul => left_res.mul(right_res),
+            // Op::Div => left_res.div(right_res),
+            // Op::Mod => left_res.rem(right_res),
+            // Op::Pow => left_res.pow(right_res),
+            // Op::Equal => left_res.eq(right_res),
+            // Op::NotEqual => left_res.ne(right_res),
+            // Op::Less => left_res.lt(right_res),
+            // Op::LessEqual => left_res.lt(right_res),
+            // Op::Greater => left_res.gt(right_res),
+            // Op::GreaterEqual => left_res.gt(right_res),
+            // Op::And => left_res.and(right_res),
+            // Op::Or => left_res.or(right_res),
+            Op::Not => Err(WalrusError::UnknownError {
                 message: format!("Operator '{}' requires one operand", op),
             })?,
+            _ => {
+                return Err(WalrusError::UnknownError {
+                    message: format!("Unknown operator: {}", op),
+                })
+            }
         }
         // fixme: the spans can cause the error span to sometimes be greater than the actual span of the
         // operation taking place because the spans get extended to the left and right of the operation
         // as we traverse the tree. its not a huge deal but it would be nice to fix
-        .map_err(|err| interpreter_err_mapper(err, &self.source_ref, span))
     }
 
     fn visit_unary_op(&mut self, op: Op, value: Node, span: Span) -> InterpreterResult {
-        let right_res = self.visit(value)?;
+        let right_res = self.interpret(value)?;
 
         match op {
             Op::Sub => right_res.neg(),
             Op::Not => right_res.not(),
-            _ => Err(GlassError::UnknownError {
+            _ => Err(WalrusError::UnknownError {
                 message: "Invalid unary operator".into(),
             })?,
         }
@@ -122,30 +135,33 @@ impl<'a> Interpreter<'a> {
         let mut map = BTreeMap::new();
 
         for (key, value) in dict {
-            map.insert(self.visit(*key)?, self.visit(*value)?);
+            map.insert(self.interpret(*key)?, self.interpret(*value)?);
         }
 
-        Ok(Value::Dict(map))
+        let key = self.arenas.insert_dict(map);
+        Ok(Value::Dict(key))
     }
 
     fn visit_list(&mut self, list: Vec<Box<Node>>) -> InterpreterResult {
         let mut vec = vec![];
 
         for node in list {
-            vec.push(self.visit(*node)?);
+            vec.push(self.interpret(*node)?);
         }
 
-        Ok(Value::List(vec))
+        let key = self.arenas.insert_list(vec);
+        Ok(Value::List(key))
     }
 
-    fn visit_anon_fn_def(&self, args: Vec<String>, body: Node) -> InterpreterResult {
+    fn visit_anon_fn_def(&mut self, args: Vec<String>, body: Node) -> InterpreterResult {
         let fn_name = format!("anon_{}", Uuid::new_v4());
-        Ok(Value::Function(fn_name, args, body))
+        let key = self.arenas.insert_function(fn_name, args, body);
+        Ok(Value::Function(key))
     }
 
     fn visit_fn_def(&mut self, name: String, args: Vec<String>, body: Node) -> InterpreterResult {
-        self.scope
-            .define(name.clone(), Value::Function(name.clone(), args, body)); // fixme: avoid clone
+        let key = self.arenas.insert_function(name.clone(), args, body);
+        self.scope.define(name, Value::Function(key));
 
         Ok(Value::Void)
     }
@@ -157,5 +173,30 @@ impl<'a> Interpreter<'a> {
             src: self.source_ref.source().into(),
             filename: self.source_ref.filename().into(),
         })
+    }
+
+    fn add(&mut self, a: Value, b: Value, span: Span) -> InterpreterResult {
+        match (a, b) {
+            (Value::Int(a), Value::Int(b)) => Ok(Value::Int(a + b)),
+            (Value::Float(FloatOrd(a)), Value::Float(FloatOrd(b))) => {
+                Ok(Value::Float(FloatOrd(a + b)))
+            }
+            (Value::String(a), Value::String(b)) => {
+                let a_str = self.arenas.get_string(a)?;
+                let b_str = self.arenas.get_string(b)?;
+                let res = a_str.clone() + b_str.as_str();
+                let key = self.arenas.insert_string(res);
+
+                Ok(Value::String(key))
+            }
+            (a, b) => Err(WalrusError::InvalidOperation {
+                op: Op::Add,
+                left: a.get_type().into(),
+                right: b.get_type().into(),
+                span,
+                src: self.source_ref.source().into(),
+                filename: self.source_ref.filename().into(),
+            })?,
+        }
     }
 }
