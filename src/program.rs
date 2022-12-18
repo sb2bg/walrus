71a6ee718eef8e239746bd3c0d03b5a0b04d228a
sb2bg <sullivanbognar@gmail.com>
2022-12-18 00:32:20 -0600

    Accept owned strings Still no working REPL source update

--- src/program.rs
+++ src/program.rs
@@ -1,4 +1,4 @@
-use crate::ast::NodeKind;
+use crate::ast::{Node, NodeKind};
 use crate::error::parser_err_mapper;
 use crate::grammar::{ProgramParser, ReplParser};
 use crate::interpreter::Interpreter;
@@ -7,32 +7,26 @@ use crate::WalrusResult;
 use log::debug;
 use std::io::{stdout, BufRead, Write};
 
-pub struct Program<'a> {
-    source_ref: SourceRef<'a>,
+pub struct Program {
+    src: String,
+    filename: String,
 }
 
-impl<'a> Program<'a> {
-    pub fn new(src: &'a str, filename: &'a str) -> Self {
-        Self {
-            source_ref: SourceRef::new(src, filename),
-        }
+impl Program {
+    pub fn new(src: String, filename: String) -> Self {
+        Self { src, filename }
     }
 
     pub fn run(&mut self) -> WalrusResult {
-        let src = self.source_ref.source();
-        let filename = self.source_ref.filename();
-
-        debug!("Read {} bytes from '{}'", src.len(), filename);
+        debug!("Read {} bytes from '{}'", self.src.len(), self.filename);
 
         let ast = *ProgramParser::new()
-            .parse(self.source_ref.source())
-            .map_err(|err| {
-                parser_err_mapper(err, self.source_ref.source(), self.source_ref.filename())
-            })?;
+            .parse(&self.src)
+            .map_err(|err| parser_err_mapper(err, &self.src, &self.filename))?;
 
-        debug!("AST > {:?}", collect_ast(ast.kind()));
+        debug!("AST > {:?}", ast);
 
-        let mut interpreter = Interpreter::new(self.source_ref);
+        let mut interpreter = Interpreter::new(&self.src, &self.filename);
         let result = interpreter.interpret(ast)?;
         debug!("Result > {:?}", result);
 
@@ -46,7 +40,7 @@ pub struct Repl<'a> {
 
 impl Repl<'_> {
     pub fn new() -> Self {
-        let interpreter = Interpreter::new(SourceRef::new("", "REPL"));
+        let interpreter = Interpreter::new("", "REPL");
 
         Self { interpreter }
     }
@@ -63,9 +57,9 @@ impl Repl<'_> {
                 .parse(&line)
                 .map_err(|err| parser_err_mapper(err, &line, "REPL"))?;
 
-            debug!("AST > {:?}", collect_ast(ast.kind()));
+            debug!("AST > {:?}", ast);
 
-            self.interpreter.set_source_ref(SourceRef::new("", "REPL")); // fixme: no source for REPL errors
+            self.interpreter.set_source_ref(""); // fixme: set source for REPL errors
 
             let result = self.interpreter.interpret(ast)?;
             debug!("{:?}", result);
@@ -82,18 +76,6 @@ impl Repl<'_> {
 
     fn prompt_and_flush() {
         print!("REPL > ");
-        stdout().flush().unwrap();
+        stdout().flush().unwrap(); // fixme: handle error even though it's unlikely to happen
     }
-}
-
-fn collect_ast(ast: &NodeKind) -> Vec<&NodeKind> {
-    let mut nodes = vec![];
-
-    if let NodeKind::Statements(stmts) = ast {
-        for stmt in stmts {
-            nodes.push(stmt.kind());
-        }
-    }
-
-    nodes
 }
