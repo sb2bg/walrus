789270d89ce900dd92180828974e01d52513f09c
sb2bg <sullivanbognar@gmail.com>
2023-05-04 23:32:56 -0500

    Add Opts enum for CLI

--- src/program.rs
+++ src/program.rs
@@ -5,19 +5,26 @@ use std::path::PathBuf;
 
 use log::debug;
 
+use crate::ast::Node;
 use crate::error::{parser_err_mapper, WalrusError};
 use crate::grammar::ProgramParser;
 use crate::interpreter::{Interpreter, InterpreterResult};
 use crate::source_ref::{OwnedSourceRef, SourceRef};
 use crate::value::Value;
 use crate::vm::compiler::BytecodeEmitter;
-use crate::vm::opcode::Opcode;
 use crate::vm::VM;
+
+#[derive(Debug, Clone, Copy)]
+pub enum Opts {
+    Compile,
+    Interpret,
+    Disassemble,
+}
 
 pub struct Program {
     source_ref: Option<OwnedSourceRef>,
     parser: ProgramParser,
-    interpreted: bool,
+    opts: Opts,
     loaded_modules: HashSet<String>,
 }
 
@@ -25,7 +32,7 @@ impl Program {
     pub fn new(
         file: Option<PathBuf>,
         parser: Option<ProgramParser>,
-        interpreted: bool,
+        opts: Opts,
     ) -> Result<Self, WalrusError> {
         let source_ref = match file {
             Some(file) => Some(get_source(file)?),
@@ -36,7 +43,7 @@ impl Program {
             source_ref,
             parser: parser.unwrap_or_else(ProgramParser::new),
             loaded_modules: HashSet::new(),
-            interpreted,
+            opts,
         })
     }
 
@@ -57,19 +64,36 @@ impl Program {
             .parse(source_ref.source())
             .map_err(|err| parser_err_mapper(err, source_ref.source(), source_ref.filename()))?;
 
-        let result = if self.interpreted {
-            let mut interpreter = Interpreter::new(source_ref, self);
-            interpreter.interpret(ast)?
-        } else {
-            debug!("Size of Opcode = {}", std::mem::size_of::<Opcode>());
-
-            let mut emitter = BytecodeEmitter::new(source_ref);
-            emitter.emit(ast)?;
-            let mut vm = VM::new(source_ref, emitter.instruction_set());
-            vm.run()?
-        };
+        let result = match self.opts {
+            Opts::Compile => self.compile(ast, source_ref),
+            Opts::Interpret => self.interpret(ast, source_ref),
+            Opts::Disassemble => self.disassemble(ast, source_ref),
+        }?;
 
         Ok(result)
+    }
+
+    fn compile(&self, ast: Node, source_ref: SourceRef) -> InterpreterResult {
+        let mut emitter = BytecodeEmitter::new(source_ref);
+        emitter.emit(ast)?;
+        let mut vm = VM::new(source_ref, emitter.instruction_set());
+        vm.run()?;
+
+        Ok(Value::Void)
+    }
+
+    fn interpret(&self, ast: Node, source_ref: SourceRef) -> InterpreterResult {
+        let mut interpreter = Interpreter::new(source_ref, self);
+        interpreter.interpret(ast)
+    }
+
+    fn disassemble(&self, ast: Node, source_ref: SourceRef) -> InterpreterResult {
+        let mut emitter = BytecodeEmitter::new(source_ref);
+        emitter.emit(ast)?;
+
+        println!("{}", emitter.instruction_set());
+
+        Ok(Value::Void)
     }
 
     const REPL_FILENAME: &'static str = "<repl>";
@@ -110,11 +134,10 @@ impl Program {
 
     pub fn load_module(&mut self, module_name: &str) -> InterpreterResult {
         if self.loaded_modules.contains(module_name) {
-            return Ok(ValueKind::Void);
+            return Ok(Value::Void);
         }
 
-        // fixme: don't pass None
-        let mut program = Program::new(Some(PathBuf::from(module_name)), None, self.interpreted)?;
+        let mut program = Program::new(Some(PathBuf::from(module_name)), None, self.opts)?;
         let result = program.execute()?;
 
         self.loaded_modules.insert(module_name.to_string());
