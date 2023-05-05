e3d41b7b7ffc8aa65ba50f7545e81fd59bee4ae7
sb2bg <sullivanbognar@gmail.com>
2023-05-04 23:34:02 -0500

    Create custom iterators for iterable values

---
+++ src/iter.rs
@@ -0,0 +1,88 @@
+use std::collections::VecDeque;
+use std::ops::Range;
+
+use rustc_hash::FxHashMap;
+
+use crate::arenas::{HeapValue, ValueHolder};
+use crate::range::RangeValue;
+use crate::value::Value;
+
+pub trait ValueIterator {
+    fn next(&mut self, arena: &mut ValueHolder) -> Option<Value>;
+}
+
+pub struct RangeIter {
+    range: Range<i64>,
+}
+
+impl RangeIter {
+    pub fn new(range: RangeValue) -> Self {
+        Self {
+            range: range.start..range.end,
+        }
+    }
+}
+
+impl ValueIterator for RangeIter {
+    fn next(&mut self, _: &mut ValueHolder) -> Option<Value> {
+        Some(Value::Int(self.range.next()?))
+    }
+}
+
+pub struct StrIter {
+    chars: VecDeque<char>,
+}
+
+impl StrIter {
+    pub fn new(string: String) -> Self {
+        Self {
+            chars: string.chars().collect(),
+        }
+    }
+}
+
+impl ValueIterator for StrIter {
+    fn next(&mut self, arena: &mut ValueHolder) -> Option<Value> {
+        self.chars
+            .pop_front()
+            .map(|c| arena.push(HeapValue::String(&c.to_string())))
+    }
+}
+
+pub struct DictIter {
+    dict: Vec<(Value, Value)>,
+}
+
+impl DictIter {
+    pub fn new(dict: FxHashMap<Value, Value>) -> Self {
+        Self {
+            dict: dict.into_iter().collect(),
+        }
+    }
+}
+
+impl ValueIterator for DictIter {
+    fn next(&mut self, arena: &mut ValueHolder) -> Option<Value> {
+        self.dict
+            .pop()
+            .map(|(k, v)| arena.push(HeapValue::Tuple(&vec![k, v])))
+    }
+}
+
+pub struct CollectionIter {
+    collection: VecDeque<Value>,
+}
+
+impl CollectionIter {
+    pub fn new(collection: Vec<Value>) -> Self {
+        Self {
+            collection: collection.into(),
+        }
+    }
+}
+
+impl ValueIterator for CollectionIter {
+    fn next(&mut self, _: &mut ValueHolder) -> Option<Value> {
+        self.collection.pop_front()
+    }
+}
